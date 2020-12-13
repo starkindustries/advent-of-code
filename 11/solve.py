f16d3e2f70d4b97d6aba009a1ecf0a39becb0a9b
Zion <zionperez@gmail.com>
2020-12-12 22:36:55 -0700

    Add solution for day 11.

---
+++ 11/solve.py
@@ -0,0 +1,96 @@
+
+filename = "sample.txt"
+filename = "input.txt"
+
+seatMap = []
+with open(filename, 'r') as handle:
+    for line in handle:
+        line = line.strip()
+        seatMap.append(line)
+
+def occupiedAdjacentSeats(row, col, seatMap):
+    occupied = 0
+    # row above
+    if row > 0:
+        # directly above
+        if seatMap[row-1][col] == "#":
+            occupied += 1 
+        # diag: above, left
+        if col > 0:
+            if seatMap[row-1][col-1] == "#":
+                occupied += 1
+        # diag: above, right
+        if col < len(seatMap[0])-1:
+            if seatMap[row-1][col+1] == "#":
+                occupied += 1
+    # row below
+    if row < len(seatMap)-1:
+        # directly below
+        if seatMap[row+1][col] == "#":
+            occupied += 1
+        # diag: below, left
+        if col > 0:
+            if seatMap[row+1][col-1] == "#":
+                occupied += 1
+        # diag: below, right
+        if col < len(seatMap[0])-1:
+            if seatMap[row+1][col+1] == "#":
+                occupied += 1
+    # col left
+    if col > 0:
+        # directly left
+        if seatMap[row][col-1] == "#":
+            occupied += 1    
+    # col right
+    if col < len(seatMap[0])-1:
+        # directly right
+        if seatMap[row][col+1] == "#":
+            occupied += 1
+    return occupied           
+
+def countOccupiedSeats(seatMap):
+    count = 0
+    for row in seatMap:
+        count += row.count("#")
+    return count
+
+def applyRules(seatMap):
+    newMap = []
+    for i in range(len(seatMap)):
+        newRow = ""
+        for j in range(len(seatMap[0])):
+            # row, col = i, j
+            seat = seatMap[i][j]
+            occupied = occupiedAdjacentSeats(i, j, seatMap)
+            if seat == "L" and occupied == 0:
+                newRow += "#"
+            elif seat == "#" and occupied >= 4:
+                newRow += "L"
+            elif seat == ".":
+                newRow += "."
+            else:
+                newRow += seat
+        newMap.append(newRow)
+    return newMap
+
+def stateChanged(map1, map2):
+    for r1, r2 in zip(map1, map2):
+        if r1 != r2:
+            return True
+    return False
+
+def printMap(myMap):
+    for line in myMap:
+        print(line)
+
+map1 = seatMap
+map2 = applyRules(seatMap)
+while stateChanged(map1, map2):
+    map1 = map2
+    map2 = applyRules(map1)
+    # print("m1:")
+    # printMap(map1)
+    # print("m2:")
+    # printMap(map2)
+count = countOccupiedSeats(map1)    
+print(f"Seating stabilized! Occupied seats: {count}")
---
+++ 11/solve2.py
@@ -0,0 +1,100 @@
+
+filename = "sample.txt"
+filename = "input.txt"
+
+seatMap = []
+with open(filename, 'r') as handle:
+    for line in handle:
+        line = line.strip()
+        seatMap.append(line)
+
+height = len(seatMap)
+width = len(seatMap[0])
+
+def checkSeatHelper(seatMap, row, col, i, occupied):
+    seat = seatMap[row][col]
+    if seat == ".":
+        return (False, i+1, occupied)
+    elif seat == "#":
+        return (True, 0, occupied+1)
+    elif seat == "L":
+        return (True, 0, occupied)
+
+def occupiedAdjacentSeats(row, col, seatMap):
+    occupied = 0    
+    # directly above
+    i, seatFound = 1, False
+    while row-i >= 0 and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row-i, col, i, occupied)        
+    # diag: above, left
+    i, seatFound = 1, False    
+    while row-i >= 0 and col-i >= 0 and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row-i, col-i, i, occupied)
+    # diag: above, right
+    i, seatFound = 1, False    
+    while row-i >= 0 and col+i < width and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row-i, col+i, i, occupied)
+    # directly left
+    i, seatFound = 1, False
+    while col-i >= 0 and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row, col-i, i, occupied)
+    # directly right
+    i, seatFound = 1, False
+    while col+i < width and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row, col+i, i, occupied)        
+    # directly below
+    i, seatFound = 1, False
+    while row+i < height and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row+i, col, i, occupied)        
+    # diag: below, left
+    i, seatFound = 1, False    
+    while row+i < height and col-i >= 0 and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row+i, col-i, i, occupied)
+    # diag: below, right
+    i, seatFound = 1, False    
+    while row+i < height and col+i < width and not seatFound:
+        seatFound, i, occupied = checkSeatHelper(seatMap, row+i, col+i, i, occupied)    
+    return occupied
+
+def countOccupiedSeats(seatMap):
+    count = 0
+    for row in seatMap:
+        count += row.count("#")
+    return count
+
+def applyRules(seatMap):
+    newMap = []
+    for i in range(len(seatMap)):
+        newRow = ""
+        for j in range(len(seatMap[0])):
+            # row, col = i, j
+            seat = seatMap[i][j]
+            occupied = occupiedAdjacentSeats(i, j, seatMap)
+            if seat == "L" and occupied == 0:
+                newRow += "#"
+            elif seat == "#" and occupied >= 5:
+                newRow += "L"
+            elif seat == ".":
+                newRow += "."
+            else:
+                newRow += seat
+        newMap.append(newRow)
+    return newMap
+
+def stateChanged(map1, map2):
+    for r1, r2 in zip(map1, map2):
+        if r1 != r2:
+            return True
+    return False
+
+def printMap(myMap):
+    for line in myMap:
+        print(line)
+
+map1 = seatMap
+map2 = applyRules(seatMap)
+while stateChanged(map1, map2):
+    map1 = map2
+    map2 = applyRules(map1)
+count = countOccupiedSeats(map1)    
+print(f"Seating stabilized! Occupied seats: {count}")
