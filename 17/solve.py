b2daacb28cc314a01c54f3e1358e00ebf809da8c
Zion <zionperez@gmail.com>
2021-01-09 12:27:21 -0700

    Add solution for day 17 part 2.

--- 17/solve.py
+++ 17/solve.py
@@ -29,41 +29,62 @@ import itertools
 #     s3 = inactive_neighbors(three_states[2], active_states)
 #     return set(s1) & set(s2) & set(s3)
 
+def add_delta(coordinate, delta):
+    # print(f"ADD DELTA: {coordinate} || {delta}")
+    new_coord = []
+    for i in range(len(coordinate)):
+        # print(f"i: {i}")
+        new_coord.append(coordinate[i] + delta[i])
+    # print(f"NEW COORD: {new_coord}")
+    return tuple(new_coord)
+
+def is_origin(coordinate):
+    for i in coordinate:
+        if i != 0:
+            return False
+    return True
 
 def simulate_cycle(states, dimensions):
-    # assert dimensions in [3, 4]
+    coordinate_values = []
+    for i in range(dimensions):
+        coordinate_values.append([s[i] for s in states])
+    # x_values = [s[0] for s in states]
+    # y_values = [s[1] for s in states]
+    # z_values = [s[2] for s in states]
 
+    coordinate_ranges = []
+    for i in range(dimensions):
+        temp = list(range(min(coordinate_values[i])-1, max(coordinate_values[i])+2))
+        coordinate_ranges.append(temp)
 
-    # for i in range(dimensions):
-    x_values = [s[0] for s in states]
-    y_values = [s[1] for s in states]
-    z_values = [s[2] for s in states]
-
-    x_range = range(min(x_values)-1, max(x_values)+2)
-    y_range = range(min(y_values)-1, max(y_values)+2)
-    z_range = range(min(z_values)-1, max(z_values)+2)
-
-    coordinates = itertools.product(x_range, y_range, z_range)
+    # x_range = range(min(x_values)-1, max(x_values)+2)
+    # y_range = range(min(y_values)-1, max(y_values)+2)
+    # z_range = range(min(z_values)-1, max(z_values)+2)
+    
+    # https://stackoverflow.com/questions/3034014/how-to-apply-itertools-product-to-elements-of-a-list-of-lists
+    # https://stackoverflow.com/questions/400739/what-does-asterisk-mean-in-python
+    coordinates = itertools.product(*coordinate_ranges)
 
     # Iterate through all cubes, save all new active states
     new_states = set()
-    for x, y, z in coordinates:
+    for c in coordinates:
         # Iterate through all neighbors, count active neighbors
         neighbors = 0
-        for dx, dy, dz in itertools.product([-1, 0, 1], repeat=3):
-            if not (dx == dy == dz == 0):
-                if (x+dx, y+dy, z+dz) in states:
+        for delta in itertools.product([-1, 0, 1], repeat=dimensions):
+            if not is_origin(delta): # e.g. if not (dx == dy == dz == 0):
+                neighbor = add_delta(c, delta)
+                if neighbor in states:
                     neighbors += 1
         # If a cube is active and exactly 2 or 3 of its neighbors
         # are also active, the cube remains active. Otherwise,
         # the cube becomes inactive.
-        if (x, y, z) in states and (neighbors == 2 or neighbors == 3):
-            new_states.add((x, y, z))
+        if c in states and (neighbors == 2 or neighbors == 3):
+            new_states.add(c)
         # If a cube is inactive but exactly 3 of its neighbors are
         # active, the cube becomes active. Otherwise, the cube
         # remains inactive.
-        if (x, y, z) not in states and neighbors == 3:
-            new_states.add((x, y, z))
+        if c not in states and neighbors == 3:
+            new_states.add(c)
 
     return new_states
 
@@ -101,7 +122,6 @@ def simulate_cycle(states, dimensions):
 
 
 def parse_input(filename, dimensions):
-    assert dimensions in [3, 4]
     with open(filename, 'r') as handle:
         puzzle_input = [line.strip() for line in handle]
     for p in puzzle_input:
@@ -118,18 +138,19 @@ def parse_input(filename, dimensions):
     return active_states
 
 
-def solve_part1(filename):
-    active_states = parse_input(filename, 3)
+def solve(filename, dimensions):
+    assert dimensions in [3, 4]    
+    active_states = parse_input(filename, dimensions)
 
     for _ in range(6):
-        active_states = simulate_cycle(active_states, 3)
+        active_states = simulate_cycle(active_states, dimensions)
 
     print(active_states)
     print(f"Active states: {len(active_states)}")
     return len(active_states)
 
+assert solve("sample.txt", 3) == 112
+assert solve('input.txt', 3) == 362
 
-assert solve_part1("sample.txt") == 112
-assert solve_part1('input.txt') == 362
-
-print(f"Part 1: {solve_part1('input.txt')} active states")
+print(f"Part 1: {solve('input.txt', 3)} active states")
+print(f"Part 2: {solve('input.txt', 4)} active states")
