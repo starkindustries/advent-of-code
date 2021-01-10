88afbe9c5fcfb384eb93f79549bfcd524db8966d
Zion <zionperez@gmail.com>
2021-01-10 13:05:55 -0700

    Add working solution for day 19 part 1.

--- 19/solve.py
+++ 19/solve.py
@@ -1,6 +1,5 @@
 
 
-
 def parse_input(filename):
     read_rules = True
     rules = {}
@@ -12,7 +11,7 @@ def parse_input(filename):
                 read_rules = False
             elif read_rules:
                 number, temp_description = line.split(":")
-                number = int(number)                
+                number = int(number)
                 description = []
                 for sub_rule in temp_description.split("|"):
                     sub_rule = sub_rule.strip()
@@ -29,49 +28,56 @@ def parse_input(filename):
     return rules, messages
 
 
-def follow_rule(i, rules):
-    # valid_messages = []
-    return follow_helper(i, rules)
-
+# Sample rules:
 # 0: 1 2
 # 1: "a"
 # 2: 1 3 | 3 1
 # 3: "b"
 
-# i = 1
-# messages = [a,]
-def follow_helper(i, rules):
+
+def follow_rule(i, rules):
+
     rule = rules[i]
     # for rule in rules.values():
-    if len(rule) == len(rule[0]) == 1 and rule[0][0] in ['a', 'b']:
+    if rule[0][0] in ['a', 'b']:
         # messages = [x.append(rule[0][0]) for x in messages]
-        print(rule[0][0])
-    elif len(rule) == 1:
-        for r in rule[0]:
-            follow_helper(r, rules)
-    else: # len(rules) == 2
-        print(f"SEPARATOR")
-        for sub in rule: # rule = 1 3 | 3 1
-            for j in sub: # sub = 1 3
-                follow_helper(j, rules)
-            print("** OR **")
-    return -1
+        # print(rule[0][0])
+        return [rule[0][0]]
+    else:
+        messages = []
+        # Process one side at a time
+        for side in rule:
+            # process each rule in each side
+            temp = ['']
+            for side_rule in side:
+                result = follow_rule(side_rule, rules)
+                temp = [t + r for t in temp for r in result]
+            messages.extend(temp)
+        return messages
 
 
 def solve_part1(filename):
     rules, messages = parse_input(filename)
-    print(rules)
-    print(messages)
+    # print(rules)
+    # print(messages)
 
     valid_messages = follow_rule(0, rules)
+    # print("MESSAGES")
+    # print(valid_messages)
+
+    count = 0
+    for m in messages:
+        if m in valid_messages:
+            count += 1
+    print(f"Valid message count: {count}")
+    return count
     # n = 0
     # while True:
     #     rule = rules[0]
     #     for item in rule:
 
 
-solve_part1("sample1.txt")
-print(f"=============================")
-solve_part1("sample2.txt")
+assert solve_part1("sample1.txt") == 2
+assert solve_part1("sample2.txt") == 2
 
-# assert solve_part1("sample1.txt") == 2
+solve_part1("input.txt")
