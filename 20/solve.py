73e94c6e4b4c06f6bedab4185959ff0bfcaed67f
Zion <zionperez@gmail.com>
2021-01-27 22:43:58 -0700

    Add partial code with some tests for day 20.

--- 20/solve.py
+++ 20/solve.py
@@ -1,25 +1,11 @@
 import math
+import pprint
 
-
-
-# imageLen = Count the number of tiles and get the sq root
-# for i in range(tileCount):
-#   for o in orientations:
-#     place a new tile (graph, tiles, position)
-#     if no valid tiles exist
-#       return False
-# print graph
-# return graph
-
-# first tile can be any orientation or any tile
-
-
-#
-# blank graph. place a tile (any tile, any orientation)
-# update graph
-# remove tile from tiles list.
-# recurse.
-
+# *******************
+# Constants
+# *******************
+TOP, RIGHT, BOTTOM, LEFT = 0, 1, 2, 3
+num_orientations = 8
 
 # *******************
 # Orientations
@@ -36,153 +22,118 @@ import math
 # 3) 4 1 2 3 => left top right bottom
 # flipped:
 # 4) 1 4 3 2 => left2 bottom2 right2 top2
-# 5) 4 3 2 1 => c2 b2 a2 d2
-# 6) 3 2 1 4 => b2 a2 d2 c2
+# 5) 4 3 2 1 => bottom2 right2 top2 left2
+# 6) 3 2 1 4 => right2 top2 left2 bottom2
 # 7) 2 1 4 3 => top2 left2 bottom2 right2
-def get_edge(tile, orientation, side):
-    if orientation == 0:
-        return tile
-    elif orientation == 1:
-        return 
-    elif orientation == 2:
-        pass
-    elif orientation == 3:
-        pass
-    elif orientation == 4:
-        pass
-    elif orientation == 5:
-        pass
-    elif orientation == 6:
-        pass
-    elif orientation == 7:
-        pass
-# def placeTile(graph, tiles, position):
-#   check adjacent tiles for this position
-#   
-#   if no valid tiles exist:
-#      return False
-#   return graph
 
 
+class Tile:
+    def __init__(self, image, o=None):
+        self.orientation = o
+        self.image = image[:]
+        self.top = image[0]
+        self.bottom = image[9]
+        self.left, self.right = "", ""
+        for row in image:
+            self.left += row[0]
+            self.right += row[9]
 
-    
-# graph is an array of tuples: (tile, orientation)
-# tile is an array of edges (strings)
-# orientation is a number from 1 to 8, as noted in "Orientations" section
+    def get_side(self, side):
+        if self.orientation == 0:
+            return [self.top, self.right, self.bottom, self.left][side]
+        elif self.orientation == 1:
+            return [self.right, self.bottom, self.left, self.top][side]
+        elif self.orientation == 2:
+            return [self.bottom, self.left, self.top, self.right][side]
+        elif self.orientation == 3:
+            return [self.left, self.top, self.right, self.bottom][side]
+        elif self.orientation == 4:
+            return [self.left[::-1], self.bottom[::-1], self.right[::-1], self.top[::-1]][side]
+        elif self.orientation == 5:
+            return [self.bottom[::-1], self.right[::-1], self.top[::-1], self.left[::-1]][side]
+        elif self.orientation == 6:
+            return [self.right[::-1], self.top[::-1], self.left[::-1], self.bottom[::-1]][side]
+        elif self.orientation == 7:
+            return [self.top[::-1], self.left[::-1], self.bottom[::-1], self.right[::-1]][side]
 
-# def solve(tile, )
-#   if not tiles:
-#      return graph
-#   if no valid tiles:
-#      return False
-#   place tile
-def assemble(tiles, graph=[], tiles_dict, edges, side_len):
+    def matches(self, side1, tile2, side2):
+        return self.get_side(side1) == tile2.get_side(side2)
+
+
+def tile_valid(graph, tile_num, orientation, tiles, side_len):
+    # if graph is empty, then any tile is valid
     if not graph:
-        for num, _ in tiles_dict.items():
-            for o in orientations:
-                tiles.remove(num)
-                result = assemble(tiles, [(num, o)], tiles_dict, edges)
-                if result:
-                    return result
-        raise RuntimeError("Error: No solution found.")
-    if not tiles:
-        return graph
-    
-    # place tile
-    pos = len(graph)
+        return True
 
     # check adjacent tiles for edges to match
     # 0 1 2
     # 3 4 5  above tiles: all positions > 2
     # 6 7 8  left tiles: all positions (index % 3) > 0
-
-    # check left tile
-    if (pos % side_len) > 0:
-        tile_left, orientation = graph[pos - 1]
-        edge = get_edge(tiles[tile_left], orientation)
-        matching_tiles = set(edges[edge])
-    # check above tile
-    if open_index > (side_len - 1):
-        tile_above, orientation = graph[open_index-image_len]
-        edge = get_edge(tiles[tile_above], orientation)
-        matching_tiles = matching_tiles & set(edges[edge])
-    # loop through matching tiles
-    for tile in matching_tiles:
-        new_tiles = tiles[:].remove(tile)
-        result = assemble(new_tiles, graph[:].append((tile, orientation)), tiles_dict, edges)
-        if result:
-            return result
-    # no matching tiles
-    return False
+    tile = Tile(tiles[tile_num].image, orientation)
+    pos = len(graph)
+    if pos >= side_len:  # check above tile
+        t, o = graph[pos - side_len]
+        above_tile = Tile(tiles[t].image, o)
+        if not above_tile.matches(BOTTOM, tile, TOP):
+            return False
+    if (pos % side_len) > 0:  # check left tile
+        t, o = graph[pos - 1]
+        left_tile = Tile(tiles[t].image, o)
+        if not left_tile.matches(RIGHT, tile, LEFT):
+            return False
+    return True
 
 
-def print_tile(edges, separator=" "):
-    # print top
-    for col in range(10):
-        print(edges[0][col], end=separator)
-    print()
-    # print left and right
-    for row in range(1, 9):
-        # left edge
-        print(edges[5][row], end=separator+("_"+separator)*8)
-        # right edge
-        print(edges[1][row])
-    # print bottom edge
-    for col in range(10):
-        print(edges[6][col], end=separator)
-    print()
+# graph: list of tuples: [(tile #, orientation), (t, o), ...]
+# tiles: list of tile numbers, e.g. [1951, 2311, 3079]
+# side_len: the length of the side of the full image
+
+def assemble(graph, tiles, side_len):
+    if len(tiles) > 0:
+        for t in tiles:
+            for o in range(num_orientations):
+                if tile_valid(graph, t, o, tiles, side_len):
+                    graph.append((t, o))
+                    tiles.remove(t)
+                    if assemble(graph, tiles, side_len):
+                        return graph
+        return False  # no valid graph found
 
 
-def get_edges(number, image, edges):
-    top = image[0]
-    # bottom should go from right to left
-    bottom = image[9][::-1]
-    left, right = "", ""
-    for row in range(10):
-        left += image[row][0]
-        right += image[row][9]
-    # left should go from bottom to top
-    left = left[::-1]
-    print(f"left: {left}")
-    print(f"right: {right}")
-    for i, e in enumerate([top, right, bottom, left]):
-        edges.setdefault(e, [])
-        if (number, i) not in edges[e]:
-            edges[e].append((number, i))
-
-
-# solve
-def solve(filename):
-    # Process input
-    tiles = {}
-    edges = {}
-    tile_count = 0
+def parse_input(filename):
     with open(filename, 'r') as handle:
-        tile_num = None
-        image = []
-        for line in handle:
-            line = line.strip()
-            if line == "":
-                continue
-            elif len(image) == 9:
-                print(f"LINE: {line}")
-                print(f"Tile {tile_num}:")
-                image.append(line)
-                tiles[tile_num] = image[:]
-                get_edges(tile_num, image, edges)
-                print_tile(tiles[tile_num])
-                tile_num, image = None, []
-                tile_count += 1
-            elif tile_num is None:
-                tile_num = int(line.replace(":", "").split(" ")[1])
-            else:
-                image.append(line)
-    image_len = math.sqrt(tile_count)
-    print(f"IMAGE LEN: {image_len}")
-    # tile = next(iter(tiles))
-    # graph = assemble(tiles)
-    print("EDGES")
-    print(edges)
+        lines = handle.readlines()
+
+    tiles = {}  # tiles[num] = [image rows]
+    image = []
+    tile_num = None
+    for line in lines:
+        line = line.strip()
+        if line == "":
+            continue
+        elif len(image) == 9:
+            image.append(line)
+            tiles[tile_num] = Tile(image)
+            image = []
+        elif "Tile" in line:
+            tile_num = int(line.replace(':', '').split(" ")[1])
+        else:
+            image.append(line)
+
+    pp = pprint.PrettyPrinter(indent=2)
+    for key, value in tiles.items():
+        print(key)
+        pp.pprint(value.image)
+    return tiles
 
 
-assert solve("sample.txt") == 20899048083289
+tiles = parse_input("./20/sample.txt")
+assert tiles[1427].image == ['###.##.#..', '.#..#.##..', '.#.##.#..#', '#.#.#.##.#',
+                             '....#...##', '...##..##.', '...#.#####', '.#.####.#.', '..#..###.#', '..##.#..#.']
+
+assert tile_valid([], 10, 0, tiles, 3)
+
+tiles = parse_input("./20/test1.txt")
+assert tile_valid([(1951, 0)], 2311, 0, tiles, 3)
+
+# assert solve("sample.txt") == 20899048083289
