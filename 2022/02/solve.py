7cef206b5a314d48dd41b3191948fe6a2cb3963e
Zion Perez <zion.perez@z-ky-mbp.local>
2022-12-05 19:42:10 -0700

    Update gitignore, add workspace, add 2022 01 and 02 challenges.

---
+++ 2022/02/solve.py
@@ -0,0 +1,18 @@
+
+# elves = []
+
+# filename = "input.txt"
+# with open(filename, 'r') as handle:
+#     calories = 0
+#     for line in handle:        
+#         line = line.strip()
+#         try:            
+#             calories += int(line)
+#         except Exception as e:
+#             elves.append(calories)
+#             calories = 0
+# elves.append(calories)
+
+# elves.sort(reverse=True)
+# maxcals = elves[0] + elves[1] + elves[2]
+# print("Max elf calories: ", maxcals)
