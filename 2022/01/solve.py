15805eebbedc8e85bb2d33f3e3aef2f006b93714
Zion Perez <zionperez@gmail.com>
2022-12-10 00:22:32 -0700

    Add test for day 01.

--- 2022/01/solve.py
+++ 2022/01/solve.py
@@ -1,18 +1,34 @@
 
-elves = []
 
-filename = "input.txt"
-with open(filename, 'r') as handle:
-    calories = 0
-    for line in handle:        
-        line = line.strip()
-        try:            
-            calories += int(line)
-        except Exception as e:
-            elves.append(calories)
-            calories = 0
-elves.append(calories)
+def solve(filename):
+    elves = []
+    with open(filename, 'r') as handle:
+        calories = 0
+        for line in handle:        
+            line = line.strip()
+            try:            
+                calories += int(line)
+            except Exception as e:
+                elves.append(calories)
+                calories = 0
+    elves.append(calories)
 
-elves.sort(reverse=True)
-maxcals = elves[0] + elves[1] + elves[2]
-print("Max elf calories: ", maxcals)
+    # Part 1
+    max_calories = max(elves)
+    print("Part1:", max_calories)
+
+    elves.sort(reverse=True)
+    max_top_three = elves[0] + elves[1] + elves[2]
+    print("Part2:", max_top_three)
+
+    return (max_calories, max_top_three)
+
+
+def test(path):
+    part1, part2 = solve(path + "input.txt")
+    assert part1 == 67658
+    assert part2 == 200158
+
+
+if __name__ == "__main__":
+    test("./")
--- 2022/test_2022.py
+++ 2022/test_2022.py
@@ -9,12 +9,45 @@ def module_from_file(module_name, file_path):
 
 
 def helper(day):
-    script = module_from_file("advent", f"./{day}/solve.py")
-    script.test(f"./{day}/")
+    year = 2022
+    script = module_from_file("PLACEHOLDER", f"./{year}/{day}/solve.py")
+    script.test(f"./{year}/{day}/")
+
+
+def test01():
+    helper("01")
+
+
+def test02():
+    helper("02")
+
+
+def test03():
+    helper("03")
+
+
+def test04():
+    helper("04")
+
+
+def test05():
+    helper("05")
+
+
+def test06():
+    helper("06")
+
+
+def test07():
+    helper("07")
 
 
 def test08():
     helper("08")
+
+
+def test09():
+    helper("09")
 
 
 def test10():
