b641058965b27fb742affabb4359b61b0dbcfec1
Zion Perez <zionperez@gmail.com>
2022-12-25 23:26:21 -0700

    Add partial solutions for day 22 and day 24.

--- 2022/22/solve.py
+++ 2022/22/solve.py
@@ -1,7 +1,9 @@
 import functools
 from copy import deepcopy
 
+
 filename = "sample.txt"
+filename = "input.txt"
 get_instructions = False
 map = []
 history = []
@@ -128,37 +130,55 @@ def move(position, steps, direction, map):
             history.append((x, y, DIRECTION_IMG[direction]))
     return (x, y)
 
+# directions
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
 
+# edges
 TOP_EDGE = 0
 RIGHT_EDGE = 1
 BOTTOM_EDGE = 2
 LEFT_EDGE = 3
 EDGE_STRINGS = {
-    TOP_EDGE: "TOP", 
-    RIGHT_EDGE: "RIGHT",
-    BOTTOM_EDGE: "BOTTOM",
-    LEFT_EDGE: "LEFT"
+    TOP_EDGE: "TOP_EDGE", 
+    RIGHT_EDGE: "RIGHT_EDGE",
+    BOTTOM_EDGE: "BOTTOM_EDGE",
+    LEFT_EDGE: "LEFT_EDGE"
+}
+DIR_STRINGS = {
+    RIGHT: "RIGHT",
+    DOWN: "DOWN",
+    LEFT: "LEFT",
+    UP: "UP"
 }
 def print_squares(squares):
     # squares = {} # (x, y) => square_template
+    print("squares = {")
     for section, edges in squares.items():
-        print(section)
+        print(f"{section}: {{")
         for name, edge in edges.items():
-            print(f"  {EDGE_STRINGS[name]}: {edge}")
-
+            if edge:
+                coord = edge[0]
+                flip = "FLIP" if edge[1] else "NO_FLIP"
+                direction = DIR_STRINGS[edge[2]]
+                print(f"  {EDGE_STRINGS[name]}: ({coord}, {flip}, {direction}),")
+            else:
+                print(f"  {EDGE_STRINGS[name]}: {edge},")
+        print("},")
+    print("}")
 
 ##############################
 position = map[0].index('.')
 position = (position, 0)
 
-UP = 0
-RIGHT = 1
-DOWN = 2
-LEFT = 3
+
 NUM_DIRECTIONS = 4
 LEFT_TURN = "L"
 RIGHT_TURN = "R"
 
+instructions_part2 = instructions
 direction = RIGHT
 while True:
     if len(instructions) < 1:
@@ -202,8 +222,13 @@ if filename == "input.txt":
 
 
 
+##############################
 # PART 2
-SIDE_LEN = 4
+##############################
+if filename == "sample.txt":
+    SIDE_LEN = 4
+elif filename == "input.txt":
+    SIDE_LEN = 50
 HORIZONTAL_SECTIONS = int(WIDTH / SIDE_LEN)
 VERTICAL_SECTIONS = int(HEIGHT / SIDE_LEN)
 sections = set()
@@ -255,7 +280,7 @@ for section in sections:
 #         ......#.
 # squares with two edges at non-opposite ends should join those edges to make a corner
 # repeat this a couple times
-for _ in range(20):
+for _ in range(1):
     for square, edges in squares.items():
         if len(edges) < 2:
             continue
@@ -264,26 +289,30 @@ for _ in range(20):
         # X 0       0 X          X       X
         # TOP/LEFT, TOP/RIGHT, BOT/LEFT, BOT/RIGHT
         corners = [
+            # edge1, edge2, e1_flip, e1_dir, e2_flip, e2_dir
             (TOP_EDGE, LEFT_EDGE, 0, DOWN, 0, RIGHT),
             (TOP_EDGE, RIGHT_EDGE, 1, DOWN, 1, LEFT),
             (BOTTOM_EDGE, LEFT_EDGE, 1, UP, 1, RIGHT),
             (BOTTOM_EDGE, RIGHT_EDGE, 0, UP, 0, LEFT)
         ]
         for corner in corners:
-            edge1, edge2, e1_flip, e1_dir, e2_flip, e2_dir = corner
-            if edges[edge1] and edges[edge2]:
-                e1_tuple = edges[edge1]
-                e2_tuple = edges[edge2]
-                e1_coord, _, _ = e1_tuple
-                e2_cord, _, _ = e2_tuple            
+            e1, e2, e1_flip, e1_dir, e2_flip, e2_dir = corner
+            if edges[e1] and edges[e2]:
+                e1_coord = edges[e1][0]
+                e2_coord = edges[e2][0]
                 # print("**", top_edge, lcoord, 0, DOWN)
                 # input()
-                squares[e1_tuple[0]][edge2] = (e2_cord, e1_flip, e1_dir)
-                squares[e2_tuple[0]][edge1] = (e1_coord, e2_flip, e2_dir)
+                if squares[e1_coord][e2] is None:
+                    squares[e1_coord][e2] = (e2_coord, e1_flip, e1_dir)
+                if squares[e2_coord][e1] is None:
+                    squares[e2_coord][e1] = (e1_coord, e2_flip, e2_dir)
+        # print("\nsquares")
+        # print_squares(squares)
+        # input()
 
 print("\nsquares")
 print_squares(squares)
-
+print(squares)
 # square sides = bottom north east south west top
 # start from top-left-most active side. call this side bottom
 # each side can travel to four other sides
@@ -324,4 +353,210 @@ print_squares(squares)
 # section: 10 (8, 8) Active
 # section: 11 (12, 8)
 # section: 11 (12, 8) Active
+NO_FLIP = 0
+FLIP = 1
 
+if filename == "sample.txt":
+    SQUARES = {
+        (4, 4): {
+        TOP_EDGE: ((8, 0), NO_FLIP, RIGHT), # conf
+        RIGHT_EDGE: ((8, 4), NO_FLIP, RIGHT), # conf
+        BOTTOM_EDGE: ((8, 8), FLIP, RIGHT), # conf
+        LEFT_EDGE: ((0, 4), NO_FLIP, LEFT), # conf
+        },
+        (8, 8): {
+        TOP_EDGE: ((8, 4), NO_FLIP, UP), # conf
+        RIGHT_EDGE: ((12, 8), NO_FLIP, RIGHT), # conf
+        BOTTOM_EDGE: ((0, 4), FLIP, UP), # conf
+        LEFT_EDGE: ((4, 4), FLIP, UP), # conf
+        },
+        (0, 4): {
+        TOP_EDGE: ((8, 0), FLIP, DOWN), # conf
+        RIGHT_EDGE: ((4, 4), NO_FLIP, RIGHT), # conf
+        BOTTOM_EDGE: ((8, 8), FLIP, UP), # conf
+        LEFT_EDGE: ((12, 8), FLIP, UP), # conf
+        },
+        (8, 4): {
+        TOP_EDGE: ((8, 0), NO_FLIP, UP), # conf
+        RIGHT_EDGE: ((12, 8), FLIP, DOWN), # conf
+        BOTTOM_EDGE: ((8, 8), NO_FLIP, DOWN), # conf
+        LEFT_EDGE: ((4, 4), NO_FLIP, LEFT), # conf
+        },
+        (8, 0): {
+        TOP_EDGE: ((0, 4), FLIP, DOWN), # conf
+        RIGHT_EDGE: ((12, 8), FLIP, LEFT), # conf
+        BOTTOM_EDGE: ((8, 4), NO_FLIP, DOWN), # conf
+        LEFT_EDGE: ((4, 4), NO_FLIP, DOWN), # conf
+        },
+        (12, 8): {
+        TOP_EDGE: ((8, 4), FLIP, LEFT), # conf
+        RIGHT_EDGE: ((8, 0), FLIP, LEFT), # conf
+        BOTTOM_EDGE: ((0, 4), FLIP, RIGHT), # conf
+        LEFT_EDGE: ((8, 8), NO_FLIP, LEFT), # conf
+        },
+    }
+
+# use print squares to get partial edges
+# print_squares()
+# manual override
+if filename == "input.txt":
+    SQUARES = {
+        (0, 100): {
+        TOP_EDGE: ((50, 50), NO_FLIP, RIGHT), # conf
+        # RIGHT_EDGE: ((50, 100), NO_FLIP, RIGHT), # conf
+        # BOTTOM_EDGE: ((0, 150), NO_FLIP, DOWN), # conf
+        LEFT_EDGE: ((50, 0), FLIP, RIGHT), # conf
+        },
+        (50, 100): {
+        # TOP_EDGE: ((50, 50), NO_FLIP, UP), # conf
+        RIGHT_EDGE: ((100, 0), NO_FLIP, UP), # conf
+        BOTTOM_EDGE: ((0, 150), NO_FLIP, LEFT), # conf
+        # LEFT_EDGE: ((0, 100), NO_FLIP, DOWN), # conf
+        },
+        (50, 0): {
+        TOP_EDGE: ((0, 150), NO_FLIP, RIGHT), # conf
+        # RIGHT_EDGE: ((100, 0), NO_FLIP, RIGHT), # conf
+        # BOTTOM_EDGE: ((50, 50), NO_FLIP, DOWN), # conf
+        LEFT_EDGE: ((0, 100), FLIP, RIGHT), # conf
+        },
+        (100, 0): {
+        TOP_EDGE: ((0, 150), NO_FLIP, UP), # conf
+        RIGHT_EDGE: ((50, 100), FLIP, LEFT), # conf
+        BOTTOM_EDGE: ((50, 50), NO_FLIP, LEFT), # conf
+        # LEFT_EDGE: ((50, 0), NO_FLIP, LEFT), # conf
+        },
+        (0, 150): {
+        # TOP_EDGE: ((0, 100), NO_FLIP, UP), # conf
+        RIGHT_EDGE: ((50, 100), NO_FLIP, UP), # conf
+        BOTTOM_EDGE: ((100, 0), NO_FLIP, DOWN), # conf
+        LEFT_EDGE: ((50, 0), NO_FLIP, DOWN) # conf
+        },
+        (50, 50): {
+        # TOP_EDGE: ((50, 0), NO_FLIP, UP), # conf
+        RIGHT_EDGE: ((100, 0), NO_FLIP, UP), # conf
+        # BOTTOM_EDGE: ((50, 100), NO_FLIP, DOWN), # conf
+        LEFT_EDGE: ((0, 100), NO_FLIP, DOWN), # conf
+        },
+    }
+
+
+history = []
+def move2(position, steps, direction, map):
+    x, y = position    
+    for _ in range(steps):
+        dx, dy = OFFSETS[direction]
+        new_x, new_y = x + dx, y + dy
+        # if new position is out of bounds or off the map:
+        if (new_y < 0) or (new_y >= HEIGHT) or (new_x < 0) or (new_x >= WIDTH) or map[new_y][new_x] == OFFMAP:            
+            section = (x // SIDE_LEN * SIDE_LEN, y // SIDE_LEN * SIDE_LEN)
+            print("SECTION: ", section, "x:", new_x, x, dx, "y:", new_y, y, dy)
+            adjacent_edge, flip, facing_direction = SQUARES[section][direction]
+            print(adjacent_edge, flip, facing_direction)
+            if (direction in [UP, DOWN] and facing_direction in [LEFT, RIGHT]):
+                # implement flipping from x to y
+                edge_y = adjacent_edge[1] + (new_x % SIDE_LEN)
+                if flip:
+                    edge_y = adjacent_edge[1] + (SIDE_LEN - (new_x % SIDE_LEN) - 1)
+                if facing_direction == LEFT:
+                    edge_x = adjacent_edge[0] + SIDE_LEN - 1 # right edge of adjacent_edge, facing LEFT
+                elif facing_direction == RIGHT:
+                    edge_x = adjacent_edge[0] # left edge of adjacent_edge, facing RIGHT
+            elif (direction in [LEFT, RIGHT] and facing_direction in [UP, DOWN]):
+                # shifting direction 90 degrees, map y's to x's
+                edge_x = adjacent_edge[0] + (new_y % SIDE_LEN)
+                if flip:
+                    edge_x = adjacent_edge[0] + (SIDE_LEN - (new_y % SIDE_LEN) - 1)
+                if facing_direction == UP:
+                    edge_y = adjacent_edge[1] + SIDE_LEN - 1 # bottom edge of adjacent_edge, facing UP
+                elif facing_direction == DOWN:
+                    edge_y = adjacent_edge[1] # top edge of adjacent_edge, facing DOWN
+            elif direction in [LEFT, RIGHT] and facing_direction == LEFT:
+                edge_y = adjacent_edge[1] + (new_y % SIDE_LEN)
+                if flip:
+                    edge_y = adjacent_edge[1] + (SIDE_LEN - (new_y % SIDE_LEN) - 1)
+                edge_x = adjacent_edge[0] + SIDE_LEN - 1
+            elif direction in [LEFT, RIGHT] and facing_direction == RIGHT:
+                edge_y = adjacent_edge[1] + (new_y % SIDE_LEN)
+                if flip:
+                    edge_y = adjacent_edge[1] + (SIDE_LEN - (new_y % SIDE_LEN) - 1)
+                edge_x = adjacent_edge[0]
+            elif direction in [UP, DOWN] and facing_direction == UP:
+                edge_x = adjacent_edge[0] + (new_x % SIDE_LEN)
+                if flip:
+                    edge_x = adjacent_edge[0] + (SIDE_LEN - (new_x % SIDE_LEN) - 1)
+                edge_y = adjacent_edge[1] + SIDE_LEN - 1
+            elif direction in [UP, DOWN] and facing_direction == DOWN:
+                edge_x = adjacent_edge[0] + (new_x % SIDE_LEN)
+                if flip:
+                    edge_x = adjacent_edge[0] + (SIDE_LEN - (new_x % SIDE_LEN) - 1)
+                edge_y = adjacent_edge[1]
+            else:
+                assert False
+
+            if map[edge_y][edge_x] == WALL:
+                return (x, y), direction
+            x = edge_x
+            y = edge_y
+            direction = facing_direction
+            history.append((x, y, DIRECTION_IMG[direction]))
+            print_map(True)
+            print(f"({x}, {y})")
+            input("press enter to continue..")
+            continue
+        # If new position is within map bounds:
+        new_map_position = map[y + dy][x + dx]
+        if new_map_position == WALL:
+            return (x, y), direction
+        elif new_map_position == OPEN:
+            x += dx
+            y += dy
+            history.append((x, y, DIRECTION_IMG[direction]))
+        print_map(True)
+        print(f"({x}, {y})")        
+        input("press enter to continue..")
+    return (x, y), direction
+
+
+position = map[0].index('.')
+position = (position, 0)
+
+# NUM_DIRECTIONS = 4
+# LEFT_TURN = "L"
+# RIGHT_TURN = "R"
+
+direction = RIGHT
+instructions = instructions_part2
+while True:
+    if len(instructions) < 1:
+        break
+    # move a number of tiles
+    print("steps:", steps)
+    steps, instructions = get_first_number(instructions)
+    print("steps:", steps, ", direction:", direction)
+    position, direction = move2(position, steps, direction, map)
+    # print(steps, instructions)
+    # turn 
+    if len(instructions) < 1:
+        break
+    turn = instructions[0]
+    print("turn:", turn)
+    instructions = instructions[1:]
+    if turn == LEFT_TURN:
+        direction = (direction - 1) % NUM_DIRECTIONS
+    elif turn == RIGHT_TURN:
+        direction = (direction + 1) % NUM_DIRECTIONS
+    history.append((position[0], position[1], DIRECTION_IMG[direction]))
+
+print_map(True)
+x, y = position
+x += 1
+y += 1
+print(x, y, DIR_VALUES[direction])
+answer = 1000 * y + 4 * x + DIR_VALUES[direction]
+print(answer)
+if filename == "sample.txt":
+    assert answer == 5031
+    print("SUCCESS!!!", answer)
+# 76360 too low
+# 135120 too low
+# 50486
--- 2022/24/solve.py
+++ 2022/24/solve.py
@@ -1,3 +1,254 @@
-filename = "sample.txt"
-with open(filename, "r") as handle:
-    for line in handle:
+import sys
+from copy import deepcopy
+
+
+sys.setrecursionlimit(100000)
+
+WALL = "#"
+GROUND = "."
+LEFT = "<"
+RIGHT = ">"
+UP = "^"
+DOWN = "v"
+BLIZZARD = (LEFT, RIGHT, UP, DOWN)
+WIDTH = None
+HEIGHT = None
+ELF = "E"
+
+open('map_printout.txt', 'w').close()
+
+
+def print_map(width, height, walls, blizzards, elf, to_file=False):    
+    if to_file:
+        handle = open("map_printout.txt", "a", encoding="utf-8")
+    for y in range(height):
+        row = ""
+        for x in range(width):
+            if (x, y) == elf:
+                # assert elf not in blizzards
+                # assert elf not in walls
+                row += ELF
+            elif (x, y) in walls:
+                row += WALL            
+            elif (x, y) in blizzards:
+                directions = blizzards[(x, y)]
+                if len(directions) > 1:
+                    row += str(len(directions))
+                elif len(directions) == 1:
+                    row += list(directions)[0]
+            else:
+                row += GROUND
+        
+        if to_file:
+            handle.write(row + "\n")
+        else:
+            print(row)
+    if to_file:
+        handle.write("\n")
+
+def check_blizzard_respawn(position, walls, direction):
+    if position not in walls:
+        return position
+    if direction == LEFT:
+        return (WIDTH - 2, position[1])
+    elif direction == RIGHT:
+        return (1, position[1])
+    elif direction == UP:
+        return (position[0], HEIGHT - 2)
+    elif direction == DOWN:
+        return (position[0], 1)
+    assert False
+
+
+OFFSETS = {
+    RIGHT: ( 1,  0),
+    LEFT:  (-1,  0),
+    UP:    ( 0, -1),
+    DOWN:  ( 0,  1),
+}
+
+def move_blizzards(blizzards, walls):
+    new_blizzards = {}
+    for position, directions in blizzards.items():
+        for direction in directions:
+            offset = OFFSETS[direction]
+            new_position = (position[0] + offset[0], position[1] + offset[1])
+            new_position = check_blizzard_respawn(new_position, walls, direction)
+            new_blizzards.setdefault(new_position, set())
+            new_blizzards[new_position].add(direction)
+    return new_blizzards
+
+
+def bfs_search(start, stop, blizzards, walls, width, height):
+    edge_len = 1
+    queue = [(start, blizzards, 0)]
+    distances = []
+
+    for _ in range(height):
+        distances.append([-1 for _ in range(width)])
+    start_x, start_y = start
+    distances[start_y][start_x] = 0
+    # min_steps = float("inf")
+
+    while queue:
+        elf, blizzards, steps = queue.pop(0)
+        if elf == stop:
+            return distances
+                                
+        # search for new position
+        elf_x, elf_y = elf
+        neighbors = [
+            (elf_x - 1, elf_y),
+            (elf_x + 1, elf_y),
+            (elf_x, elf_y - 1),
+            (elf_x, elf_y + 1),
+        ]
+
+        new_blizzards = move_blizzards(blizzards, walls)
+        print_map(width, height, walls, new_blizzards, elf)
+        steps += 1
+        for neighbor in neighbors:
+            if neighbor in walls or neighbor in new_blizzards \
+                    or neighbor[1] <= 0 or neighbor[1] >= HEIGHT:
+                # neighbor[1] == 0 is y at the starting point, do not consider
+                continue
+            x, y = neighbor
+            # print((x, y))
+            if distances[y][x] == -1:
+                distances[y][x] = steps
+            queue.append((neighbor, new_blizzards, steps))
+        # check if waiting will put elf in a blizzard
+        if elf not in new_blizzards and steps > 1:
+            queue.append((elf, new_blizzards, steps))
+    return distances
+
+
+def distance_between(pos1, pos2):
+    x1, y1 = pos1
+    x2, y2 = pos2
+    return abs(x2 - x1) + abs(y2 - y1)
+
+# MAX_VISITS = 5
+# visited = {}
+# min_steps = float("inf")
+# def dfs_search(elf, stop, blizzards, walls, width, height, steps):
+#     global min_steps    
+#     if steps >= min_steps:
+#         print_map(width, height, walls, blizzards, elf)
+#         return None
+#     if elf == stop:
+#         print(f"FOUND solution at {steps} steps!!")
+#         min_steps = steps
+#         return steps        
+#     if elf in walls or elf in blizzards:
+#         return None
+#     if elf in visited and visited[elf] > MAX_VISITS:
+#         return None
+#     distance = distance_between(elf, stop)
+#     if steps + distance >= min_steps:
+#         print("elf at", elf)
+#         return None
+#     if elf[1] <= 0 and steps > 0:
+#         return None
+#     # debug
+#     # print_map(width, height, walls, blizzards, elf)
+#     # input()
+    
+#     # track elf visits
+#     visited.setdefault(elf, 0)
+#     visited[elf] += 1    
+#     # find neighbors
+#     elf_x, elf_y = elf
+#     neighbors = [
+#         (elf_x - 1, elf_y),
+#         (elf_x + 1, elf_y),
+#         (elf_x, elf_y - 1),
+#         (elf_x, elf_y + 1),
+#     ]
+#     neighbors_with_distance = []
+#     for neighbor in neighbors:
+#         distance = distance_between(neighbor, stop)
+#         neighbors_with_distance.append((neighbor, distance))
+#     neighbors_with_distance.sort(key=lambda item: item[1])
+
+#     new_blizzards = move_blizzards(blizzards, walls)    
+#     steps += 1
+#     results = []    
+#     # check adjacent positions and move elf
+#     # did_find_ground = False
+#     for neighbor, _ in neighbors_with_distance:
+#         temp = dfs_search(neighbor, stop, new_blizzards, walls, width, height, steps)        
+#         if temp is not None:
+#             results.append(temp)
+#             # did_find_ground = True
+#     # if not did_find_ground:
+#     # make elf wait        
+#     temp = dfs_search(elf, stop, new_blizzards, walls, width, height, steps)
+#     if temp:
+#         results.append(temp)
+#     # results = [x for x in results if x is not None]
+#     if results:
+#         return min(results)
+#     return None
+        
+
+walls = set()
+blizzards = {}
+valley = []
+
+try:
+    filename = str(sys.argv[1])
+except Exception as e:
+    print("Warning exception:", e)
+    filename = "sample2"
+# assert filename == "input" or filename == "sample"
+filename += ".txt"
+
+with open(filename, "r") as handle:    
+    for y, line in enumerate(handle):
+        line = line.strip()
+        valley.append(line)
+        for x, ch in enumerate(line):
+            if ch == WALL:
+                walls.add((x, y))
+            elif ch == GROUND:
+                continue
+            elif ch in BLIZZARD:
+                blizzards.setdefault((x, y), set())
+                blizzards[(x, y)].add(ch)
+                # blizzards.append([ch, x, y])
+                # blizz_set.add((x, y))
+
+WIDTH = len(valley[0])
+HEIGHT = len(valley)
+start = (1, 0)
+stop = (WIDTH - 2, HEIGHT - 1)
+print(f"width: {WIDTH}, height: {HEIGHT}")
+print_map(WIDTH, HEIGHT, walls, blizzards, start)
+
+# DEBUG
+blizzards_copy = deepcopy(blizzards)
+for _ in range(20):
+    print_map(WIDTH, HEIGHT, walls, blizzards_copy, start, to_file=True)
+    blizzards_copy = move_blizzards(blizzards_copy, walls)
+# DEBUG
+
+# for i in range(10):
+#     blizzards = move_blizzards(blizzards, walls)
+#     print_map(WIDTH, HEIGHT, walls, blizzards)
+#     input("press enter to continue..")
+
+distances = bfs_search(start, stop, blizzards, walls, WIDTH, HEIGHT)
+print(distances)
+stop_x, stop_y = stop
+answer = distances[stop_y][stop_x]
+print(answer)
+
+# answer = dfs_search(start, stop, blizzards, walls, WIDTH, HEIGHT, 0)
+# print("Min distance:", distance_between(start, stop))
+# print("Result:", answer)
+if filename == "sample.txt":
+    assert answer == 18
+
+# 1019, 1011, 557, 631 too high
+# 485 not sent
